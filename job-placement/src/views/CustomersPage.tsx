8aea359293a0c32913fcf2683abf47d1d645d478
EliaFerraro <ferraro.eli.99@gmail.com>
2024-08-17 18:13:05 +0200

    Feat: Start add Interfaces to get responses from customers microservice

--- job-placement/src/views/CustomersPage.tsx
+++ job-placement/src/views/CustomersPage.tsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react";
 import { Button, Col, Row } from "react-bootstrap";
 import { BsPlus } from "react-icons/bs";
+import { PagedResponse } from "../interfaces/PagedResponse";
+import { Customer } from "../interfaces/Customer";
 
 function CustomersPage() {
 
-    const [customers, setCustomers] = useState([]);
+    const [customers, setCustomers] = useState<PagedResponse<Customer> | null>(null);
     const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
 
-        fetch("http://localhost:8082/API/customers")
+        fetch("/crmService/v1/API/customers")
             .then(res => {
                 if (!res.ok) {
                     throw new Error('/API/customers : Network response was not ok');
@@ -20,14 +23,14 @@ function CustomersPage() {
                 (result) => {
                 
                     setCustomers(result);
-
-                    console.log("Customerrrrrrrrrr: " + customers);
+                    setLoading(false);
                 
                 }
             )
             .catch((error) => {
                 setError(true);
                 console.log(error);
+                setLoading(false);
             });
 
     }, []);
@@ -52,6 +55,53 @@ function CustomersPage() {
                 <h5>An error occurred. Please, reload the page!</h5>
             </Col>
         </Row>}
+
+        { loading && <Row className="w-100">
+            <Col className="w-100 d-flex justify-content-center align-items-center mt-5">
+                <h5>Loading...</h5>
+            </Col>
+        </Row>}
+
+        {
+            !error && !loading && customers!== null && customers.totalElements === 0 && <Row className="w-100">
+                <Col className="w-100 d-flex justify-content-center align-items-center mt-5">
+                    <h5>No customers found yet! Start adding one!</h5>
+                </Col>
+            </Row>
+        }
+
+        {
+            !error && !loading && customers!== null && customers.totalElements > 0 && 
+
+            <Row className="w-100">
+                <Col className="w-100 d-flex justify-content-center align-items-center mt-3">
+                    {
+                    customers.content.map((customer, index) => {
+                        return (
+                            <Row key={index} className="w-100 border border-dark rounded-3 p-3 mb-2">
+                                <Col xs={12} md={6} lg={3}>
+                                    <h5>{customer.id}</h5>
+                                </Col>
+                                <Col xs={12} md={6} lg={3}>
+                                    <h5>{customer.id}</h5>
+                                </Col>
+                                <Col xs={12} md={6} lg={3}>
+                                    <h5>{customer.id}</h5>
+                                </Col>
+                                <Col xs={12} md={6} lg={3}>
+                                    <h5>{customer.id}</h5>
+                                </Col>
+                            </Row>
+                        )
+                    })
+                }
+
+                </Col>
+            </Row>
+
+        }
+
+
       
     </div>
   );
