954047ad8fc6b70ffaf445ae021444d252b04fea
EliaFerraro <ferraro.eli.99@gmail.com>
2024-09-27 23:23:14 +0200

    Feat: Add icons to files in professional page

--- job-placement/src/views/ProfessionalPage.tsx
+++ job-placement/src/views/ProfessionalPage.tsx
@@ -1,10 +1,35 @@
 import { useEffect, useState } from "react";
-import { Button, Col, Modal, OverlayTrigger, Row, Toast, ToastContainer, Tooltip } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Modal,
+  OverlayTrigger,
+  Row,
+  Toast,
+  ToastContainer,
+  Tooltip,
+} from "react-bootstrap";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
-import { BsChevronDown, BsChevronUp, BsPencilSquare, BsTrash } from "react-icons/bs";
+import {
+  BsChevronDown,
+  BsChevronUp,
+  BsFileEarmark,
+  BsFileEarmarkExcel,
+  BsFileEarmarkImage,
+  BsFileEarmarkPdf,
+  BsFileEarmarkPpt,
+  BsFileEarmarkText,
+  BsFileEarmarkWord,
+  BsFileEarmarkZip,
+  BsPencilSquare,
+  BsTrash,
+} from "react-icons/bs";
 import { MeInterface } from "../interfaces/MeInterface";
 import { employmentStateToText, toTitleCase } from "../utils/costants";
-import { deleteProfessional, fetchProfessional } from "../apis/ProfessionalRequests";
+import {
+  deleteProfessional,
+  fetchProfessional,
+} from "../apis/ProfessionalRequests";
 import { ProfessionalWithAssociatedData } from "../interfaces/ProfessionalWithAssociatedData";
 import { JobOfferCard } from "./JobOffersPage";
 import { DocumentFile } from "../interfaces/DocumentFile";
@@ -33,14 +58,20 @@ function ProfessionalPage({ me }: { me: MeInterface }) {
 
   const handleCloseDeleteModal = () => setShowDeleteModal(false);
 
-  const [professional, setProfessional] = useState<ProfessionalWithAssociatedData | null>(null);
+  const [professional, setProfessional] =
+    useState<ProfessionalWithAssociatedData | null>(null);
 
   const [attachments, setAttachments] = useState<DocumentFile[]>([]);
 
   const [expandedInfoSection, setExpandedInfoSection] = useState(false);
 
   useEffect(() => {
-    if (id === undefined || id === null || id === "" || Number.parseInt(id) < 1) {
+    if (
+      id === undefined ||
+      id === null ||
+      id === "" ||
+      Number.parseInt(id) < 1
+    ) {
       navigate("/not-found");
       return;
     }
@@ -59,7 +90,7 @@ function ProfessionalPage({ me }: { me: MeInterface }) {
             .catch((error) => {
               console.error("Error:", error);
             });
-        })
+        });
         setLoading(false);
       })
       .catch((error) => {
@@ -73,13 +104,23 @@ function ProfessionalPage({ me }: { me: MeInterface }) {
     <div className="w-100">
       {showAlert && (
         <ToastContainer position="top-end" className="p-3">
-          <Toast bg={success ? "success" : "danger"} show={success != null} onClose={() => (location.state = null)}>
+          <Toast
+            bg={success ? "success" : "danger"}
+            show={success != null}
+            onClose={() => (location.state = null)}
+          >
             <Toast.Header>
-              <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+              <img
+                src="holder.js/20x20?text=%20"
+                className="rounded me-2"
+                alt=""
+              />
               <strong className="me-auto">JobConnect</strong>
               <small>now</small>
             </Toast.Header>
-            <Toast.Body>{success ? "Operation correctly executed!" : "Operation failed!"}</Toast.Body>
+            <Toast.Body>
+              {success ? "Operation correctly executed!" : "Operation failed!"}
+            </Toast.Body>
           </Toast>
         </ToastContainer>
       )}
@@ -89,10 +130,21 @@ function ProfessionalPage({ me }: { me: MeInterface }) {
           <Modal.Title>Delete professional</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p style={{ color: "#856404", fontSize: "1rem", backgroundColor: "#fff3cd", padding: "10px", borderRadius: "5px" }}>
-            <strong>Warning:</strong> Are you sure you want to <strong>permanently delete</strong> this professional record? This action is{" "}
-            <strong>irreversible</strong> and will delete all <strong>associated job applications</strong>, including <strong>pending</strong> and{" "}
-            <strong>accepted</strong> ones. Linked job offers will revert to the <strong>selection phase</strong>.
+          <p
+            style={{
+              color: "#856404",
+              fontSize: "1rem",
+              backgroundColor: "#fff3cd",
+              padding: "10px",
+              borderRadius: "5px",
+            }}
+          >
+            <strong>Warning:</strong> Are you sure you want to{" "}
+            <strong>permanently delete</strong> this professional record? This
+            action is <strong>irreversible</strong> and will delete all{" "}
+            <strong>associated job applications</strong>, including{" "}
+            <strong>pending</strong> and <strong>accepted</strong> ones. Linked
+            job offers will revert to the <strong>selection phase</strong>.
           </p>
 
           <p className="text-center fs-3 fw-semibold">
@@ -100,7 +152,11 @@ function ProfessionalPage({ me }: { me: MeInterface }) {
           </p>
         </Modal.Body>
         <Modal.Footer className="justify-content-between">
-          <Button variant="secondary" className="ms-5" onClick={() => handleCloseDeleteModal()}>
+          <Button
+            variant="secondary"
+            className="ms-5"
+            onClick={() => handleCloseDeleteModal()}
+          >
             Close
           </Button>
           <Button
@@ -128,14 +184,24 @@ function ProfessionalPage({ me }: { me: MeInterface }) {
         </Col>
         {!loading && (
           <Col className="d-flex justify-content-end">
-            <OverlayTrigger overlay={<Tooltip id="editButton">Edit Professional</Tooltip>}>
-              <Button className="d-flex align-items-center primaryButton me-4" onClick={() => navigate(`/ui/professionals/${id}/edit`)}>
+            <OverlayTrigger
+              overlay={<Tooltip id="editButton">Edit Professional</Tooltip>}
+            >
+              <Button
+                className="d-flex align-items-center primaryButton me-4"
+                onClick={() => navigate(`/ui/professionals/${id}/edit`)}
+              >
                 <BsPencilSquare size={"1em"} className="me-2" />
                 Edit
               </Button>
             </OverlayTrigger>
-            <OverlayTrigger overlay={<Tooltip id="deleteButton">Delete Professional</Tooltip>}>
-              <Button className="d-flex align-items-center primaryDangerButton me-4" onClick={() => setShowDeleteModal(true)}>
+            <OverlayTrigger
+              overlay={<Tooltip id="deleteButton">Delete Professional</Tooltip>}
+            >
+              <Button
+                className="d-flex align-items-center primaryDangerButton me-4"
+                onClick={() => setShowDeleteModal(true)}
+              >
                 <BsTrash size={"1em"} className="me-2" />
                 Delete
               </Button>
@@ -158,15 +224,24 @@ function ProfessionalPage({ me }: { me: MeInterface }) {
           <div className="d-flex justify-content-between mb-2 ps-0">
             <h4 className="p-0 m-0 d-flex align-items-center">
               {`${professional?.professionalDTO.information.name} ${professional?.professionalDTO.information.surname}`}
-              <span className="fw-light fs-6 ms-2">({professional?.professionalDTO.information.ssnCode})</span>
+              <span className="fw-light fs-6 ms-2">
+                ({professional?.professionalDTO.information.ssnCode})
+              </span>
             </h4>
 
-            <span className="text-uppercase fs-4 fw-semibold" style={{ color: "#162250" }}>
-              {employmentStateToText(professional?.professionalDTO.employmentState || "")}
+            <span
+              className="text-uppercase fs-4 fw-semibold"
+              style={{ color: "#162250" }}
+            >
+              {employmentStateToText(
+                professional?.professionalDTO.employmentState || ""
+              )}
             </span>
           </div>
           <Row className="d-flex">
-            <p className="p-0 m-0 fs-6 fw-light">{professional?.professionalDTO.information.comment}</p>
+            <p className="p-0 m-0 fs-6 fw-light">
+              {professional?.professionalDTO.information.comment}
+            </p>
           </Row>
 
           {expandedInfoSection && (
@@ -178,7 +253,9 @@ function ProfessionalPage({ me }: { me: MeInterface }) {
                   </Row>
 
                   <Row className="d-flex">
-                    <p className="p-0 m-0 fs-6">{professional?.professionalDTO.geographicalLocation} </p>
+                    <p className="p-0 m-0 fs-6">
+                      {professional?.professionalDTO.geographicalLocation}{" "}
+                    </p>
                   </Row>
                 </Row>
               </Col>
@@ -190,7 +267,9 @@ function ProfessionalPage({ me }: { me: MeInterface }) {
                   </Row>
 
                   <Row className="d-flex">
-                    <p className="p-0 m-0 fs-6">{professional?.professionalDTO.dailyRate} €</p>
+                    <p className="p-0 m-0 fs-6">
+                      {professional?.professionalDTO.dailyRate} €
+                    </p>
                   </Row>
                 </Row>
               </Col>
@@ -203,11 +282,17 @@ function ProfessionalPage({ me }: { me: MeInterface }) {
                   <Row className="d-flex flex-wrap ps-2">
                     {professional?.professionalDTO.skills &&
                       professional.professionalDTO.skills.length > 0 &&
-                      professional.professionalDTO.skills.map((skill, index) => (
-                        <div key={index} style={{ width: "auto" }} className="text-truncate me-2 tag mb-1">
-                          {skill}
-                        </div>
-                      ))}
+                      professional.professionalDTO.skills.map(
+                        (skill, index) => (
+                          <div
+                            key={index}
+                            style={{ width: "auto" }}
+                            className="text-truncate me-2 tag mb-1"
+                          >
+                            {skill}
+                          </div>
+                        )
+                      )}
                   </Row>
                   {professional?.professionalDTO.skills.length === 0 && (
                     <Row className="d-flex">
@@ -225,9 +310,69 @@ function ProfessionalPage({ me }: { me: MeInterface }) {
                   <Row className="d-flex flex-wrap">
                     {attachments &&
                       attachments.length > 0 &&
-                      attachments.map((attachment, index) => (
-                        <Row key={index} className="d-flex align-items-center mb-1">
-                          <a href={`/documentStoreService/v1/API/documents/${attachment.id}/data`} target="_blank" rel="noreferrer" className="text-truncate me-2 p-0">
+                      attachments.sort((a, b) => a.name.localeCompare(b.name)).map((attachment, index) => (
+                        <Row key={index} className="d-flex mb-1">
+                          <a
+                            href={`/documentStoreService/v1/API/documents/${attachment.id}/data`}
+                            target="_blank"
+                            rel="noreferrer"
+                            className="text-truncate me-2 p-0 d-flex align-items-center"
+                          >
+                            {attachment.contentType === "application/pdf" ? (
+                              <BsFileEarmarkPdf
+                                style={{ width: "20px", height: "20px" }}
+                                className="me-1"
+                              />
+                            ) : attachment.contentType ===
+                                "application/msword" ||
+                              attachment.contentType ===
+                                "application/vnd.openxmlformats-officedocument.wordprocessingml.document" ? (
+                              <BsFileEarmarkWord
+                                style={{ width: "20px", height: "20px" }}
+                                className="me-1"
+                              />
+                            ) : attachment.contentType ===
+                                "application/vnd.ms-excel" ||
+                              attachment.contentType ===
+                                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ? (
+                              <BsFileEarmarkExcel
+                                style={{ width: "20px", height: "20px" }}
+                                className="me-1"
+                              />
+                            ) : attachment.contentType ===
+                                "application/vnd.ms-powerpoint" ||
+                              attachment.contentType ===
+                                "application/vnd.openxmlformats-officedocument.presentationml.presentation" ? (
+                              <BsFileEarmarkPpt
+                                style={{ width: "20px", height: "20px" }}
+                                className="me-1"
+                              />
+                            ) : attachment.contentType === "application/zip" ||
+                              attachment.contentType ===
+                                "application/x-rar-compressed" ? (
+                              <BsFileEarmarkZip
+                                style={{ width: "20px", height: "20px" }}
+                                className="me-1"
+                              />
+                            ) : attachment.contentType === "image/jpeg" ||
+                              attachment.contentType === "image/png" ? (
+                              <BsFileEarmarkImage
+                                style={{ width: "20px", height: "20px" }}
+                                className="me-1"
+                              />
+                            ) : attachment.contentType === "text/plain" ||
+                              attachment.contentType ===
+                                "application/octet-stream" ? (
+                              <BsFileEarmarkText
+                                style={{ width: "20px", height: "20px" }}
+                                className="me-1"
+                              />
+                            ) : (
+                              <BsFileEarmark
+                                style={{ width: "20px", height: "20px" }}
+                                className="me-1"
+                              />
+                            )}
                             {attachment.name}
                           </a>
                         </Row>
@@ -245,7 +390,11 @@ function ProfessionalPage({ me }: { me: MeInterface }) {
 
           <Row className="d-flex justify-content-center">
             <Col className="d-flex justify-content-center align-items-center">
-              <div className="text-center fs-6 text-muted" style={{ cursor: "pointer" }} onClick={() => setExpandedInfoSection(!expandedInfoSection)}>
+              <div
+                className="text-center fs-6 text-muted"
+                style={{ cursor: "pointer" }}
+                onClick={() => setExpandedInfoSection(!expandedInfoSection)}
+              >
                 {expandedInfoSection ? (
                   <>
                     <BsChevronUp size={"1em"} />
@@ -276,11 +425,13 @@ function ProfessionalPage({ me }: { me: MeInterface }) {
         </Row>
       )}
 
-      {!loading && professional?.jobofferDTOS && professional?.jobofferDTOS.length === 0 && (
-        <Row className="d-flex flex-column text-center mt-5">
-          <h4>No job offers found yet!</h4>
-        </Row>
-      )}
+      {!loading &&
+        professional?.jobofferDTOS &&
+        professional?.jobofferDTOS.length === 0 && (
+          <Row className="d-flex flex-column text-center mt-5">
+            <h4>No job offers found yet!</h4>
+          </Row>
+        )}
 
       {!loading &&
         professional?.jobofferDTOS.map((joboffer) => (
